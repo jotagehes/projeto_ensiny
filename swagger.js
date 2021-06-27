c8bc69314937bc562039b0a1df43aa37eeb35c3a
jotagehes <62728338+jotagehes@users.noreply.github.com>
2021-06-26 23:11:51 -0300

    Documentação com Swagger

--- swagger.js
+++ swagger.js
@@ -1,9 +1,21 @@
-const swaggerAutogen = require('swagger-autogen');
+const swaggerAutogen = require('swagger-autogen')();
 
 const outputFile = './swagger_arquivos.json';
 const routes = ['./index.js'];
 const docs = {
-    
+    "swagger": "2.0",
+    "info": {
+      "version": "4.4.2",
+      "title": "ENSINY API",
+      "description": "API de interação sistema ENSINY - ® all rights reserved -"
+    },
+    "host": "localhost:3000",
+    "basePath": "/",
+    "tags": [],
+    "schemes": [
+      "http",
+      "htpps"
+    ],
 };
 
 swaggerAutogen(outputFile, routes, docs).then(()=>{
